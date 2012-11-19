d2cd7f48015f275b8294f4a41c16c7284ae2f363
Irfan Nurhakim <myfunareaa@gmail.com>
2012-11-19 20:27:34 +0700

    add time and compression args

--- src/com/query/controller/QueryProcessor.java
+++ src/com/query/controller/QueryProcessor.java
@@ -196,20 +196,15 @@ public class QueryProcessor {
         String termMappingFileName = path + DOC_MAPPING;
 
         try {
-            // Open the file that is the first 
-            // command line parameter
             FileInputStream fstream = new FileInputStream(termMappingFileName);
-            // Get the object of DataInputStream
             DataInputStream in = new DataInputStream(fstream);
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             String strLine;
             String[] a;
-            //Read File Line By Line
             while ((strLine = br.readLine()) != null) {
                 a = strLine.split("="); 
                 tempDocMapping.put(a[0], a[1]);
             }
-            //Close the input stream
             in.close();
         } catch (Exception e) {//Catch exception if any
             System.err.println("Error: " + e.getMessage());
